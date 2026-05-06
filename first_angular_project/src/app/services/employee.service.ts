b4d0ac61d75a1ed6771ac59dc122b222795b7a35
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-05-06 14:02:13 +0600

    ghg

--- first_angular_project/src/app/services/employee.service.ts
+++ first_angular_project/src/app/services/employee.service.ts
@@ -1,17 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { EmployeeModel } from '../models/employee.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  baseApi:string="http://localhost:3000/Employee";
-//Post request
+  baseApi: string = "http://localhost:3000/Employee";
+  constructor(private http: HttpClient) { }
 
-//get Request
+  //Post request
+  saveEmployee(employee: EmployeeModel) {
+    this.http.post<EmployeeModel>(this.baseApi, employee);
 
-//Update Request
+  }
 
-//Delete Request
+  //get Request
+  getAllEmployee(): Observable<EmployeeModel[]> {
+    return this.http.get<EmployeeModel[]>(this.baseApi);
+  }
+
+  //Update Request
+
+  updateEmployee(employee: EmployeeModel): Observable<EmployeeModel> {
+
+    return this.http.put<EmployeeModel>(this.baseApi + '/' + employee.id, employee);
+  }
+  //Delete Request
+  deleteEmployee(id: string): Observable<void> {
+    return this.http.delete<void>(this.baseApi + '/' + id);
+  }
 
 
 }
